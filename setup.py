145f0a0fb00284a8d37edc211085bd7e2130bb00
vmagueta <solermvictor@gmail.com>
2024-06-17 19:48:54 -0300

    Set up projects initial organization and metadata
    - Add setup.py to turn installable
    - Add __main__ for executable
    - Add __init__ for package
    
    closes #8

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name="dundie",
+    version="0.1.0",
+    description="Reward Point System for Dundler Mifflin",
+    author="Victor Magueta",
+    packages=find_packages(),
+)
