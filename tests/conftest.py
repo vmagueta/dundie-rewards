77ad96636965a8e0091d3aab8507020f2118c7a1
vmagueta <solermvictor@gmail.com>
2024-06-21 10:05:33 -0300

    Adding finalizers, setup and teardown to tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,3 +1,6 @@
+import pytest
+
+
 MARKER = """\
 unit: Mark unit tests
 integration: Mark integration tests
@@ -9,3 +12,10 @@ low: Low priority
 def pytest_configure(config):
     for line in MARKER.split("\n"):
         config.addinivalue_line("markers", line)
+
+
+@pytest.fixture(autouse=True)
+def go_to_tmpdir(request): # injeção de dependências
+    tmpdir = request.getfixturevalue("tmpdir")
+    with tmpdir.as_cwd():
+        yield # protocolo de generators
--- tests/test_load.py
+++ tests/test_load.py
@@ -1,11 +1,49 @@
+import os
+import uuid
 import pytest
 from dundie.core import load
 from .constants import PEOPLE_FILE
 
 
+def setup_module():
+    print()
+    print("roda antes dos testes desse modulo\n")
+
+
+def teardown_module():
+    print()
+    print("vai rodar apos os testes desse modulo\n")
+
+
+@pytest.fixture(scope="function", autouse=True)
+def create_new_file(tmpdir):
+    file_ = tmpdir.join("new_file.txt")
+    file_.write("isso é sujeira...")
+    yield
+    file_.remove()
+
+
 @pytest.mark.unit
 @pytest.mark.high
-def test_load():
+def test_load(request):
     """Test load function."""
+    filepath = f"arquivo indesejado-{uuid.uuid4()}.txt"
+    request.addfinalizer(lambda: os.unlink(filepath))
+
+    with open(filepath, "w") as file_:
+        file_.write("dados uteis somente par ao teste")
+
+    assert len(load(PEOPLE_FILE)) == 2
+    assert load(PEOPLE_FILE)[0][0] == 'J'
+
+
+@pytest.mark.unit
+@pytest.mark.high
+def test_load2():
+    """Test load function."""
+
+    with open(f"arquivo indesejado-{uuid.uuid4()}.txt", "w") as file_:
+        file_.write("dados uteis somente par ao teste")
+
     assert len(load(PEOPLE_FILE)) == 2
     assert load(PEOPLE_FILE)[0][0] == 'J'
